30514c961ff456deffeed0e41c49585085400a6f
risan <risanbagja@gmail.com>
2018-10-06 15:57:39 +0200

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Risan Bagja Pradana  on 06102018.
+//  Copyright © 2018 Risan Bagja Pradana . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
